5a85b680a6a7629755c6f88ef2c301563c4e563c
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-13 15:48:36 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -20,6 +20,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl restart mongod
 
+echo ""
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
