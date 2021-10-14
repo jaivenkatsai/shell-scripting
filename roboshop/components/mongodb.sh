927c0893a86de7dc7a356fa6aa729bcbc786ea41
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 16:20:06 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,7 +4,7 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-Install Mongo & Start Service.
+
 # yum install -y mongodb-org 
 # systemctl enable mongod
 # systemctl start mongod
