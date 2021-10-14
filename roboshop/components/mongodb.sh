a5bf5de095bba5652b151303fd9c1f80d8c4fbec
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 15:59:39 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -14,11 +14,7 @@ fi
 
 echo "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
-if [ $? -eq 0]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-fi
+
 
 echo "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
