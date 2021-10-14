2c12adad558ce5653c43785b40121176f87feb1d
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 16:09:43 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -10,11 +10,9 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 
 yum install -y mongodb-org &>>/tmp/log
 
-
-
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
- systemctl enable mongod
+systemctl enable mongod
 systemctl restart mongod
 
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
