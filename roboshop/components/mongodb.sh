950a11e93c3fd08176a01699d47bf2b23a69c11b
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 17:21:28 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -48,6 +48,11 @@ fi
 cd /tmp
 echo "Extacting Schema Archive"
 unzip -o mongodb.zip &>>/tmp/log
+if [ $ -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+fi
 cd mongodb-main 
  
 echo "Loading Schema"
