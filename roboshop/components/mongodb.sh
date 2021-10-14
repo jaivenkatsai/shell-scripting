2ebc75b13ccc7ebb91f5bb29ce4b0f15714c1415
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 15:35:05 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -25,6 +25,6 @@ echo "Extarcting Schema Archive"
 unzip -o mongodb.zip >>/tmp/log
 cd mongodb-main 
 echo "Loading Schema"
-mongo < catalogue.js >/tmp/log
-mongo < users.js >/tmp/log
+mongo < catalogue.js >>/tmp/log
+mongo < users.js >>/tmp/log
   
