2b352e02bda16c34302a2919fea143b43cae68d0
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 17:08:24 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -17,7 +17,7 @@ echo "Starting MongoDB"
 systemctl enable mongod
 systemctl restart mongod
 
-echo "Downloading MongoDB S"
+echo "Downloading MongoDB Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
 cd /tmp
