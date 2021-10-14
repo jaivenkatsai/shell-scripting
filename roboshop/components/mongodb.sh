64bb0fb7a1e7fa82c3d95b7be81de538a02dc4af
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 15:52:59 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -9,7 +9,7 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 if [ $? -eq 0]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
-    echo -e "\e[31m\e[0m"
+    echo -e "\e[31mFAILURE\e[0m"
 fi
 
 echo "Installing MongoDB"
