ff055c9372feb5d84becbaf4e1ad9539233916e9
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-14 15:31:34 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,7 +8,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 
 echo "Installing MongoDB"
-yum install -y mongodb-org 
+yum install -y mongodb-org >/tmp/log
 
 echo "Configuring MongoDB"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
@@ -22,7 +22,7 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 
 cd /tmp
 echo "Extarcting Schema Archive"
-unzip -o mongodb.zip 
+unzip -o mongodb.zip >/tmp/log
 cd mongodb-main 
 echo "Loading Schema"
 mongo < catalogue.js >/tmp/log
