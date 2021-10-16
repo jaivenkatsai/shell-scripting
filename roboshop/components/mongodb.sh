97d4349260ba49b93e0774885cdc1879af827a18
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 10:04:55 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -44,8 +44,4 @@ cd mongodb-main
 echo "Loading Schema"
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js  &>>/tmp/log
-if [ $? -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-fi
+Status_Check $?
