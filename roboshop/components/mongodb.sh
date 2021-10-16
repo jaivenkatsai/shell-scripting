56a47333ba8879402e0ffd6c2818779f5f620635
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 10:29:00 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -26,11 +26,11 @@ Print "Installing MongoDB\t"
 yum install -y mongodb-org &>>/tmp/log
 Status_Check $?
 
-Print "Configuring MongoDB/t"
+Print "Configuring MongoDB\tt"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 Status_Check $?
 
-Print "Starting MongoDB/t"
+Print "Starting MongoDB\t"
 systemctl enable mongod
 systemctl restart mongod
 Status_Check $?
