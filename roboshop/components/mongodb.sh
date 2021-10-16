4d86ace4d4f485deb27721046fc1f3febfde3a19
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 10:25:08 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -22,7 +22,7 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
 Status_Check $?
 
-Print "Installing MongoDB    /t"
+Print "Installing MongoDB/t"
 yum install -y mongodb-org &>>/tmp/log
 Status_Check $?
 
@@ -30,7 +30,7 @@ Print "Configuring MongoDB/t"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 Status_Check $?
 
-Print "Starting MongoDB/t"
+Print "Starting MongoDB/t   "
 systemctl enable mongod
 systemctl restart mongod
 Status_Check $?
