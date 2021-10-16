b875ce83b808e09dd393a93c9297c96e34ddee21
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 11:06:08 +0530

    --- Auto-Git Commit ---

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -13,7 +13,7 @@ gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mong
 Status_Check $?
 
 Print "Installing MongoDB\t"
-yum install -y mongodb-org &>>/tmp/log
+yum install -y mongodb-org &>>$LOG
 Status_Check $?
 
 Print "Configuring MongoDB\t"
@@ -31,7 +31,7 @@ Status_Check $?
 
 cd /tmp
 Print "Extacting Schema Archive"
-unzip -o mongodb.zip &>>/tmp/log
+unzip -o mongodb.zip &>>$LOG
 Status_Check $?
 cd mongodb-main 
  
