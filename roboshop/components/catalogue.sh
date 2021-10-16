d088d1321ef226d57f9ba2d22534a4ceb495e4d4
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 14:37:13 +0530

    --- Auto-Git Commit ---

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -31,4 +31,4 @@ Status_Check $?
 
 chown roboshop:roboshop -R /home/roboshop
 
-mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue  && systemctl enable catalogue
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service && systemctl daemon-reload && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
