410b50ab0d887cf637b82bfceb3c8a606c709c98
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-16 10:33:41 +0530

    --- Auto-Git Commit ---

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+Status_Check() {
+    if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+fi
+}
+
+Print() {
+    echo -n -e "$1 \t- "
+}
