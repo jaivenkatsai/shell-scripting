92a53f705e5be8b5bb1bf151c53fe16358f3095b
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-17 15:43:50 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -10,7 +10,7 @@ Print "Setup Redis Repos"
 # yum-config-manager --enable remi &>>$LOG
 Status_Check $?
 
-Print "In"
+Print "Install Redis"
 # yum install redis -y
 Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
