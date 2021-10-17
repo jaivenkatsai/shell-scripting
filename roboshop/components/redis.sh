f9fd5ddb5fcb91455a13d90fdefce84bf502f62d
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-17 15:39:18 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,6 +3,7 @@
 source components/common.sh
 
 Print "Install Yum Utils & Download Redis Repos"
+yum install epel-release yum-utils -y
 # yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 # yum-config-manager --enable remi
 # yum install redis -y
