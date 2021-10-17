0c57bb2c48f349e7cd2369777d69e1f1747d09bf
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-17 15:40:18 +0530

    --- Auto-Git Commit ---

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 
 Print "Install Yum Utils & Download Redis Repos"
-yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &
 # yum-config-manager --enable remi
 # yum install redis -y
 Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
