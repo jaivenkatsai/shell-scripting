c76454d78b30c95530dfcf44fc0784940794b4ee
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-18 16:04:58 +0530

    --- Auto-Git Commit ---

--- roboshop/components/create-instance.sh
+++ roboshop/components/create-instance.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id --image-id ami-0abcdef1234567890 \
-    --instance-type t2.micro \
+aws ec2 run-instances --image-id --image-id ami-0abcdef1234567890 --instance-type t2.micro
     --key-name MyKeyPair
