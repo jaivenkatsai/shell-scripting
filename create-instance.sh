1f7e02d604c1ee069795371e87b9b97fa8fab61b
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-20 18:48:36 +0530

    --- Auto-Git Commit ---

--- create-instance.sh
+++ create-instance.sh
@@ -13,4 +13,5 @@ aws ec2 describe-instances --filters "Name=tag:Name,Values=catalogue" | jq .Rese
 if [ $? -eq 0 ]; then
     echo "Instance $INSTANCE_NAME is already running"
     exit 0
+fi
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
