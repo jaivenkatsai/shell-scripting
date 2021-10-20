4deb8d4e860038b988e74aa64c873e823c225b7e
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-20 18:46:35 +0530

    --- Auto-Git Commit ---

--- create-instance.sh
+++ create-instance.sh
@@ -10,5 +10,5 @@ if [ -z "${INSTANCE_NAME}" ]; then
 fi
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=catalogue" | jq .Reservations[].Instances[].State.Name | grep running &>/dev/null
-if [ $? -]
+if [ $? -eq 0 ]
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
