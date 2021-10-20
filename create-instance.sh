1c4a5c95261e787ba700a860030ead7d2436b017
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-20 09:38:40 +0530

    --- Auto-Git Commit ---

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,4 @@
 LID="lt-0ff4d1f08d27a230e"
 LVER=2
 
-aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER
+aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$
