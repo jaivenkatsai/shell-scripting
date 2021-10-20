0436106acc0fc13fc11d0a09f32d61ec78ea335b
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-20 11:35:22 +0530

    --- Auto-Git Commit ---

--- create-instance.sh
+++ create-instance.sh
@@ -5,6 +5,7 @@ LVER=2
 INSTANCE_NAME=$1
 
 if [ -z "${INSTANCE_NAME}" ]; then
-    echo ""
+    echo "Input is missing"
+    exit 
 
 InstanceId=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "Resource Type=spot-instances-request,Tags=[{Key=Name,| jq .Instances[].InstanceId | sed -e 's"//g')
