ba70fb94a376a54db61327d366d71a16e56f2756
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-10-20 11:35:11 +0530

    --- Auto-Git Commit ---

--- create-instance.sh
+++ create-instance.sh
@@ -5,6 +5,6 @@ LVER=2
 INSTANCE_NAME=$1
 
 if [ -z "${INSTANCE_NAME}" ]; then
-    echo
+    echo ""
 
 InstanceId=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "Resource Type=spot-instances-request,Tags=[{Key=Name,| jq .Instances[].InstanceId | sed -e 's"//g')
