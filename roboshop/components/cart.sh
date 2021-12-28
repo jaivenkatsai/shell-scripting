e2e26d9e7e59b94a838ef2bc74ac6c39c798b723
jaivenkatsai <venkatpulivarthi88@gmail.com>
2021-12-28 11:53:56 +0530

    --- Auto-Git Commit ---

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -5,7 +5,7 @@ source components/common.sh
 COMPONENT=cart
 ## NODEJS is a function from common.sh
 NODEJS
-#<<<<<<< HEAD
-#=======
-#@© 2021 GitHub, Inc.
-#>>>>>>> 15f62d9612ea07cf8b75b8abe232d3283b07466a
+<<<<<<< HEAD
+=======
+© 2021 GitHub, Inc.
+>>>>>>> 15f62d9612ea07cf8b75b8abe232d3283b07466a
